52f6197f5ef4d9118513a7b6ebda196fe9342cf6
div-antk <51283468+owaii@users.noreply.github.com>
2020-08-29 22:30:59 +0900

    slick導入

---
+++ carousel/js/common.js
@@ -0,0 +1,19 @@
+// $(function() {
+//   $('.slider').slick({
+//     infinite: true,
+//     dots:true,
+//     slidesToshow: 1,
+//     centerMode: true,
+//     cenerPadding: '100px',
+//     autoplay: true,
+//     responsive: [{
+//       breakpoint: 480,
+//         settings: {
+//           centerMode: false,
+//         }
+//     }]
+//   });
+// });
+$(document).ready(function(){
+  $('.slider').slick();
+});
